4f76bbecc81aa9238662483203c83f6679bed516
Evan Astle <starphys@gmail.com>
2023-01-27 20:03:12 -0800

    Gfx hello world, basic vector class.

---
+++ vec3.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+using std::sqrt;
+
+class vec3 {
+public:
+	vec3() : e{ 0,0,0 } {}
+	vec3(double e0, double e1, double e2) : e{ e0, e1, e2 } {}
+	// vec3(vec3& vector) : e{ *vector.e } {}
+
+	// Get coordinate values
+	double x() const { return e[0]; }
+	double y() const { return e[1]; }
+	double z() const { return e[2]; }
+
+	// Get negative vector
+	vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
+	
+	// Access values by index
+	double operator[](int i) const { return e[i]; }
+	double& operator[](int i) { return e[i]; }
+
+	// Vector addition
+	vec3& operator+=(const vec3& v) {
+		e[0] += v.e[0];
+		e[1] += v.e[1];
+		e[2] += v.e[2];
+		return *this;
+	}
+
+	// Scalar multiplication and division
+	vec3& operator*=(const double t) {
+		e[0] *= t;
+		e[1] *= t;
+		e[2] *= t;
+		return *this;
+	}
+
+	vec3& operator/=(const double t) {
+		return *this *= 1 / t;
+	}
+
+	// Magnitude
+	double length() const {
+		return sqrt(length_squared());
+	}
+
+	double length_squared() const {
+		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+	}
+
+	double e[3];
+};
+
+//Type aliases
+using point3 = vec3;
+using color = vec3;
